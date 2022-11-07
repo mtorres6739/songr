334589bbcdb7bc75bab65366b3ff32fd1301beca
Mathew Torres <torres.mathew@gmail.com>
2022-11-07 16:10:19 -0700

    inital commit from spring init

---
+++ src/test/java/com/mtorres6739songr/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtorres6739songr.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
